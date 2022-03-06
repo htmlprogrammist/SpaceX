3d1f373e4ff5aed985c121f197503b06132724af
Egor Badmaev <badmaeve2511@gmail.com>
2022-03-06 14:41:11 +0300

    Initial Commit

---
+++ SpaceX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX
+//
+//  Created by Егор Бадмаев on 06.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
